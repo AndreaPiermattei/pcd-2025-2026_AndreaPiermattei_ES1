66eaf56f8c5b53227f4d374cf12a76781af608e7
Andrea Piermattei - andrea.piermattei@studio.unibo.it <andrea.piermattei@studio.unibo.it>
2026-04-13 14:14:40 +0200

    starting point ass.1

---
+++ assignments/assignment-01/src/pcd/mainApplicationAssignmentOne/MainAppAssignmentOne.java
@@ -0,0 +1,7 @@
+package pcd.mainApplicationAssignmentOne;
+
+public class MainAppAssignmentOne {
+    public static void main(String[] argv) {
+        System.err.println("esecuzione Assignment one  v0");
+    }
+}

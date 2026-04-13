37efcb6b13ff8332de32dc104865e3e1f7d7f217
Andrea Piermattei - andrea.piermattei@studio.unibo.it <andrea.piermattei@studio.unibo.it>
2026-04-13 15:36:47 +0200

    refactoring

--- assignments/assignment-01/src/pcd/mainApplicationAssignmentOne/model/MonitorUpdateBalls.java
+++ assignments/assignment-01/src/pcd/mainApplicationAssignmentOne/model/MonitorUpdateBalls.java
@@ -7,8 +7,6 @@ import pcd.sketch01.Boundary;
 
 public class MonitorUpdateBalls {
 
-    private int checkpoint1 = 0;
-    private int checkpoint2 = checkpoint1+1;
     private List<Ball> passiveBallsOnBoard;    
     private Boundary boundsOfBoard;
     

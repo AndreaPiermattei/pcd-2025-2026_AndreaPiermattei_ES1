2ea828e942bae87351667f41fe02db989d5edd19
AndreaPiermattei <andrea.piermattei@studio.unibo.it>
2026-05-16 21:16:48 +0200

    working on task version

---
+++ assignments/assignment-01/src/pcd/mainApplicationAssignmentoneTask/tasks/CollisionCheckTask.java
@@ -0,0 +1,11 @@
+package pcd.mainApplicationAssignmentoneTask.tasks;
+
+public class CollisionCheckTask implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
